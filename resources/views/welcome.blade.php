77af0130a7cde6692c6176cdc311c0d017137300
Cristobal <mondmaxy@gmail.com>
2016-05-06 11:45:31 -0300

    remove react

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,39 +13,10 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
 
 
-    <!--- React -->
-    <script src="js/build/react.js"></script>
-    <script src="js/build/react-dom.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.24/browser.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
 
-    <script type="text/babel">
-
-        /*var ExampleApplication = React.createClass({
-            render: function () {
-                var elapsed = Math.round(this.props.elapsed / 100);
-                var seconds = elapsed / 10 + (elapsed % 10 ? '' : '.0' );
-                var message =
-                        'React has been successfully running for ' + seconds + ' seconds.';
-
-                return
-                <
-                p > {message} < / p >;
-            }
-        });
-        var start = new Date().getTime();
-        setInterval(function () {
-            ReactDOM.render(
-            < ExampleApplication
-            elapsed = {new Date().getTime() - start} / >,
-                    document.getElementById('container')
-            )
-            ;
-        }, 50);*/
-
-    </script>
 </head>
 <body id="top" class="scrollspy">
 
