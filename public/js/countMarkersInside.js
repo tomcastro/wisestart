844858177d9dd597f279d9beeb2b30253bfe7d2c
cristobal <mondmaxy@gmail.com>
2016-06-09 00:20:34 -0400

    mapa

--- public/js/countMarkersInside.js
+++ public/js/countMarkersInside.js
@@ -1,6 +1,5 @@
 function countMarkersInside(polygon, markers){
 	let markersInside = 0;
-	console.log(markers);
         for(let value of markers)
         {
             if(google.maps.geometry.poly.containsLocation(value.position, polygon))
