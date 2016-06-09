0082bcae3e4272c698daf5bb51ae5746469dd6b7
cristobal <mondmaxy@gmail.com>
2016-06-09 14:55:15 -0400

    las vistas vienen de app.blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,23 +1,20 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
-    <meta name="theme-color" content="#2196F3">
-    <title>EmprendeYA!</title>
+@extends('layouts.app')
+@section('scripts')
+<script src="min/plugin-min.js"></script>
+<script src="min/custom-min.js"></script>
+<script src="js/gmaps.js"></script>
+<script src="js/area.js"></script>
+<script src="js/getPolygonsBy.js"></script>
+<script src="js/countMarkersInside.js"></script>
+<script src="js/colorByMarkers.js" ></script>
+<script src="js/mapa.js"></script>
+<script src="js/searchForPlaces.js"></script>
+<script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDa8MJk2VV9QBkzjnEratB11lpuxkcxgyE&libraries=places&callback=initMap">
+</script>
+@endsection
 
-    <!-- CSS  -->
-    <link href="min/plugin-min.css" type="text/css" rel="stylesheet">
-    <link href="min/custom-min.css" type="text/css" rel="stylesheet">
-    <!-- Compiled and minified CSS -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-
-  <!-- Compiled and minified JavaScript -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
-      
-    
-</head>
+@section('body')
 <body id="top" class="scrollspy">
 
 <!-- Pre Loader -->
@@ -28,9 +25,6 @@
     <div class="loader-section section-right"></div>
 
 </div>
-
-<!--Navigation-->
-@include('partials.navbar')
 
 <!--Hero-->
 <div class="section no-pad-bot" id="index-banner">
@@ -148,25 +142,5 @@
 </div>
 
 @include('partials.team')
-
-<!--Footer-->
-@include('partials.footer')
-@include('modals.report_modal')
-
-
-<!--  Scripts-->
-<script src="min/plugin-min.js"></script>
-<script src="min/custom-min.js"></script>
-<script src="js/gmaps.js"></script>
-<script src="js/area.js"></script>
-<script src="js/getPolygonsBy.js"></script>
-<script src="js/countMarkersInside.js"></script>
-<script src="js/colorByMarkers.js" ></script>
-<script src="js/mapa.js"></script>
-<script src="js/searchForPlaces.js"></script>
-<script async defer
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDa8MJk2VV9QBkzjnEratB11lpuxkcxgyE&libraries=places&callback=initMap">
-</script>
-
 </body>
-</html>
+@endsection
